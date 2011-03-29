aaaf5b94d53deeeb1c52a4a026e7e2af02a410cf
shebanian <info@shebanian.com>
2011-03-28 22:20:38 -0700

    Edited public/js/app.js via GitHub

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,3 @@
     return $("#flash").delay(2000).slideUp('slow');
   });
 }).call(this);
-
-var focusOnDiv;
-focusOnDiv = function(divId) {
-  var divElement;
-  divElement = document.getElementById(divId);
-  if (divElement !== null && typeof divElement !== 'undefined') {
-    return divElement.focus();
-  }
-};
